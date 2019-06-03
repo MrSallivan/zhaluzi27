b608ba1de27a0890a7f3ae9d1d25c5c9769cb7b9
MrSallivan <nik.sallivan@gmail.com>
2019-06-03 12:44:15 +1000

    коммит в ветку02.06.19

--- js/slider-work.js
+++ js/slider-work.js
@@ -121,7 +121,56 @@ $(document).ready(function(){
 
 $(document).ready(function () {
     $('.slide-content').slick({
-        autoplay: true
+        infinite: true,
+          speed: 300,
+          accessibility: true,
+          adaptiveHeight: true,
+          autoplay: true,
+          prevArrow: $('.arrow-left'),
+          nextArrow: $('.arrow-right'),
+          responsive: [{
+              breakpoint: 10000,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+              }
+            },
+            {
+              breakpoint: 1170,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 850,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              }
+            },
+            {
+              breakpoint: 750,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 320,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 });
 $('.arrow-right').on('click', function () {
